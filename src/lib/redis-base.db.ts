b991fb7c2caa95931f3a283011b5b1f6f5db9887
p14774298 <p14774298@gmail.com>
2025-09-15 08:27:53 +0000

    4

--- src/lib/redis-base.db.ts
+++ src/lib/redis-base.db.ts
@@ -479,8 +479,10 @@ export abstract class BaseRedisStorage implements IStorage {
     }
   }
 
-  // ---------- Cache ----------
-  private cacheKey(key: string) { return `cache:${encodeURIComponent(key)}`; }
+  // ---------- 通用缓存方法 ----------
+  private cacheKey(key: string) {
+    return `cache:${key}`;
+  }
 
 
 
